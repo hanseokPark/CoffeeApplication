41ede1239e1cc0e6f4127951b23e4772b9729523
hanseok Park <es12850@naver.com>
2018-05-11 15:35:19 +0900

    DTO

--- src/main/java/kr/or/dgit/coffee/Dto/Product.java
+++ src/main/java/kr/or/dgit/coffee/Dto/Product.java
@@ -1,29 +1,47 @@
 package kr.or.dgit.coffee.Dto;
 
 public class Product {
-	private char code;
+	private String code;
 	private String name;
 	
 	
-	private Product() {
-		// TODO Auto-generated constructor stub
+	private Product() {}
+	
+	private Product(String code, String name) {
+		this.code = code;
+		this.name = name;
 	}
-	public char getCode() {
-		return code;
-	}
-	public void setCode(char code) {
+
+	private Product(String code) {
 		this.code = code;
 	}
+
+	public String getCode() {
+		return code;
+	}
+
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+
 	public String getName() {
 		return name;
 	}
+
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
+
 	@Override
 	public String toString() {
 		return String.format("Product [code=%s, name=%s]", code, name);
 	}
+	
+	
 	
 
 }	
--- src/main/java/kr/or/dgit/coffee/Dto/Sale.java
+++ src/main/java/kr/or/dgit/coffee/Dto/Sale.java
@@ -2,51 +2,79 @@ package kr.or.dgit.coffee.Dto;
 
 public class Sale {
 	private int no;
-	private char code;
+	private String code;
 	private int price;
-	private int saleCnt;
-	private int marginRate;
+	private int saleCnt; //판매수량
+	private int marginRate; //마진율
+	private Product product; //제품
 	
-	
-	private Sale() {
-		// TODO Auto-generated constructor stub
+	private Sale() {}
+
+	private Sale(int no) {
+		this.no = no;
 	}
+
+	private Sale(int no, int saleCnt, int marginRate, Product product) {
+		this.no = no;
+		this.saleCnt = saleCnt;
+		this.marginRate = marginRate;
+		this.product = product;
+	}
+
 	public int getNo() {
 		return no;
 	}
+
 	public void setNo(int no) {
 		this.no = no;
 	}
-	public char getCode() {
+
+	public String getCode() {
 		return code;
 	}
-	public void setCode(char code) {
+
+	public void setCode(String code) {
 		this.code = code;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
 		this.price = price;
 	}
+
 	public int getSaleCnt() {
 		return saleCnt;
 	}
+
 	public void setSaleCnt(int saleCnt) {
 		this.saleCnt = saleCnt;
 	}
+
 	public int getMarginRate() {
 		return marginRate;
 	}
+
 	public void setMarginRate(int marginRate) {
 		this.marginRate = marginRate;
 	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
 	@Override
 	public String toString() {
-		return String.format("Sale [no=%s, code=%s, price=%s, saleCnt=%s, marginRate=%s]", no, code, price, saleCnt,
-				marginRate);
+		return String.format("Sale [no=%s, code=%s, price=%s, saleCnt=%s, marginRate=%s, product=%s]", no, code, price,
+				saleCnt, marginRate, product);
 	}
 	
 	
-
+	
 }
